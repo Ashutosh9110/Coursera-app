8067db7d90d1a649d7d6adb53a06ecbb15f90955
Ashutosh9110 <ashusingh19911082@gmail.com>
2024-09-24 10:40:21 +0530

    Step 5: Connection to MongoDb

---
+++ db.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+console.log("connected to MongoDb")
+const ObjectId = mongoose.Types.ObjectId;
+
+mongoose.connect("mongodb+srv://venividivici:weDrGVvFIUh1ZsE0@cluster0.e1dex.mongodb.net/coursera-app")
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    email : { type: String, unique: true },
+    password : String,
+    firstName : String,
+    lastNamse : String,
+});
+
+const adminSchema = new Schema({
+    email : { type: String, unique: true },
+    password : String,
+    firstName : String,
+    lastNamse : String,
+})
+
+const courseSchema = new Schema({
+    title : String,
+    description : String,
+    price : Number,
+    imageUrl : String,
+    creatorId : ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId : ObjectId, // the userId refers to the userSchema
+    courseId : ObjectId, // the course id refers to the courseSchema
+
+});
+
+const userModel = mongoose.model("user", userSchema)
+const adminModel = mongoose.model("admin", adminSchema)
+const purchaseModel = mongoose.model("purchase", courseSchema)
+const courseModel = mongoose.model("course", purchaseSchema)
+
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
